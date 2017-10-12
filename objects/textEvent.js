30c7f2df374f81dd17da949095e3bd73522d55a4
Raymond Chiu <a00952787@se14l0120-05.ad.bcit.ca>
2017-10-11 23:48:15 -0700

    added enter functionality on locations, wrote text writing function, attempted to write DOM element

--- objects/textEvent.js
+++ objects/textEvent.js
@@ -3,6 +3,8 @@ var person1text = ["This is bullshit",
             "Please make it stop", 
             "It's already dead!"
            ];
+        var style = { font: "24px Arial", fill: "#ffffff", align: "center" };        
+        var text = null;
 
 class testEvent extends abstractObject {
 
@@ -20,18 +22,52 @@ class testEvent extends abstractObject {
             this.createFromTiledObject(element, this.textEvents);
             this.game.physics.enable(element, Phaser.Physics.ARCADE);
         }, this);
+        this.enterBut = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     }
 
     updateThis(game, player) {
         super.updateThis(game, player);
         this.game.physics.arcade.overlap(this.player, this.textEvents.children, this.readText, null, this);
-
     }
 
     readText() {
 
+        if (this.enterBut.isDown){
+            text = this.game.add.text(0, 0, person1text[3], style);
+            text.fixedToCamera = true;
+            text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 
+            //  We'll set the bounds to be from x0, y100 and be 800px wide by 100px high
+            text.setTextBounds(0, 100, 800, 100);
+            
+        }
     }
+    
+    printing(person, lineNum) {
 
+        var letter = 0;
+        var id = setInterval(frame, 20);
+        function frame() {
+            if (letter >= person[lineNum].length) {
+                clearInterval(id);
+            } else {
+                var addLetters = text.substring(0,letter);
+                tester.innerText = addLetters;
+                letter++; 
 
+            }
+          }
+    }    
+   
 }
+
+var theBody = document.getElementById("container");
+
+function makeABox(){
+    var tBox = document.createElement("div");
+    tBox.width = "90vw";
+    tBox.height = "20vw";
+    theBody.appendChild(tBox); 
+}
+
+
