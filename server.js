3566032c98930425c2f4c0da98b187784c709bd9
spencergrayp <Spencer.Gray@prizmmedia.com>
2018-02-06 20:54:10 -0800

    tried to fix directory paths

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
-app.use("/phaser",express.static(__dirname + "/node_modules/phaser"));
-app.use("/assets",express.static(__dirname + "/assets"));
-app.use("/state",express.static(__dirname + "/state"));
-app.use("/objects",express.static(__dirname + "/objects"));
-app.use("/misc",express.static(__dirname + "/misc"));
+//app.use("/phaser",express.static(__dirname + "/node_modules/phaser"));
+//app.use("/assets",express.static(__dirname + "/assets"));
+//app.use("/state",express.static(__dirname + "/state"));
+//app.use("/objects",express.static(__dirname + "/objects"));
+//app.use("/misc",express.static(__dirname + "/misc"));
+//console.log(__dirname);
+
+app.use("/phaser",express.static("/node_modules/phaser"));
+app.use("/assets",express.static("/assets"));
+app.use("/state",express.static("/state"));
+app.use("/objects",express.static("/objects"));
+app.use("/misc",express.static("/misc"));
 console.log(__dirname);
 
 app.get('/',function(req,res){
