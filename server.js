085f1663782364c39f9d14faf792252b89797634
spencergrayp <Spencer.Gray@prizmmedia.com>
2018-02-06 20:58:49 -0800

    same

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ var server = require('http').Server(app);
 //app.use("/misc",express.static(__dirname + "/misc"));
 //console.log(__dirname);
 
-app.use("/phaser",express.static("D3PG_Overworld/node_modules/phaser"));
-app.use("/assets",express.static("D3PG_Overworld/assets"));
-app.use("/state",express.static("D3PG_Overworld/state"));
-app.use("/objects",express.static("D3PG_Overworld/objects"));
-app.use("/misc",express.static("D3PG_Overworld/misc"));
-console.log(__dirname);
-
 app.get('/',function(req,res){
-    res.sendFile('/index.html');
+    res.sendFile(__dirname+'/index.html');
 });
 
 server.listen(8080,function(){ // Listens to port 8080
