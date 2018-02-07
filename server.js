a4634c8750321891b8c36b31d81357fc050e5d66
spencergrayp <Spencer.Gray@prizmmedia.com>
2018-02-06 21:04:49 -0800

    reverted

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
-app.use("/phaser",express.static(__dirname + "/D3PG_Overworld/node_modules/phaser"));
-app.use("/assets",express.static(__dirname + "/D3PG_Overworld/assets"));
-app.use("/state",express.static(__dirname + "/D3PG_Overworld/state"));
-app.use("/objects",express.static(__dirname + "/D3PG_Overworld/objects"));
-app.use("/misc",express.static(__dirname + "/D3PG_Overworld/misc"));
+app.use("/phaser",express.static(__dirname + "/node_modules/phaser"));
+app.use("/assets",express.static(__dirname + "/assets"));
+app.use("/state",express.static(__dirname + "/state"));
+app.use("/objects",express.static(__dirname + "/objects"));
+app.use("/misc",express.static(__dirname + "/misc"));
 console.log(__dirname);
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/D3PG_Overworld/index.html');
+    res.sendFile(__dirname+'/index.html');
 });
 
 server.listen(8080,function(){ // Listens to port 8080
